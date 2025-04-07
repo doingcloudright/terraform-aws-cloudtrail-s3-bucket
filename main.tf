45ef89e21fe43ab4e7e9b767fd015be74511eecd
Maarten van der Hoef <maarten@vanderhoef.eu>
2025-04-07 12:33:09 +0200

    feat: up log storage

--- main.tf
+++ main.tf
@@ -84,7 +84,8 @@ data "aws_iam_policy_document" "default" {
 
 module "s3_bucket" {
   source  = "cloudposse/s3-log-storage/aws"
-  version = "0.17.0"
+  version = "1.4.4"
+
   enabled = module.this.enabled
 
   acl                                    = var.acl
